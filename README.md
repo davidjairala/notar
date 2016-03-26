4735a71bd72973fa942bded51eb63b439db9a984
David Jairala <davidjairala@gmail.com>
2016-03-25 21:15:53 -0500

    more better, also readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Usage
+
+Source in your .bashrc, .zshrc, etc.  Then:
+
+Open up `notar.sh` and set up `notesdir` to point to your Notes directory.
+I like to keep mine in a Dropboxed folder.
+
+* `nnote`: create a timestamped folder inside your notes folder and open
+your text editor there.
+* `nnote note_name`: create a teimstamped folder as above, but also open
+your text editor on the file `note_name.md`.
+* `notes`: open file editor on your notes folder.
+* `onotes`: open notes folder with finder.
--- notar.sh
+++ notar.sh
@@ -1,17 +1,36 @@
+#!/bin/sh
+
+editor() {
+  subl $1
+}
+
 notesdir() {
   echo "/Users/`whoami`/Dropbox/Notes/"
 }
 
 nnote() {
   foldername=$(date +%Y_%m_%d)
-  echo `notesdir`"$foldername"
-  subl `notesdir`"$foldername"
+  note_path=`notesdir`"$foldername"
+
+  # create timestamped folder
+  mkdir -p "$note_path"
+
+  if [ $# -eq 1 ]
+    then
+      # argument was supplied, open file
+      editor "$note_path"
+      editor "$note_path/$1.md"
+  else
+      # no argument, just open folder
+      editor "$note_path"
+  fi
+
 }
 
 notes() {
   subl `notesdir`
 }
 
-fnotes() {
+onotes() {
   open `notesdir`
 }
