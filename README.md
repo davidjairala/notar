0b15d8d8e0378b2e07ab0286c13c75a5e33c3b06
David Jairala <davidjairala@gmail.com>
2016-03-25 22:18:22 -0500

    better function names, readme updates

--- README.md
+++ README.md
@@ -3,11 +3,15 @@
 Source in your .bashrc, .zshrc, etc.  Then:
 
 Open up `notar.sh` and set up `notesdir` to point to your Notes directory.
-I like to keep mine in a Dropboxed folder.
+I like to keep mine in a Dropboxed folder.  You might also want to change
+`_notar_editor` into your favorite editor and `_notar_search` into your
+favorite search search tool.
 
 * `nnote`: create a timestamped folder inside your notes folder and open
 your text editor there.
 * `nnote note_name`: create a teimstamped folder as above, but also open
 your text editor on the file `note_name.md`.
 * `notes`: open file editor on your notes folder.
+* `gnotes`: cd into notes folder.
 * `onotes`: open notes folder with finder.
+* `fnotes`: use `ag` to search for notes.
--- notar.sh
+++ notar.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
-editor() {
+###########
+# Helpers #
+###########
+
+_notar_editor() {
   subl $1
 }
 
+_notar_search() {
+  ag $1 `notesdir`
+}
+
+# what's the notes dir
 notesdir() {
   echo "/Users/`whoami`/Dropbox/Notes/"
 }
 
+# new note, optional name of note param
 nnote() {
   foldername=$(date +%Y_%m)
   note_path=`notesdir`"$foldername"
@@ -15,22 +25,33 @@ nnote() {
   # create timestamped folder
   mkdir -p "$note_path"
 
-  if [ $# -eq 1 ]
-    then
-      # argument was supplied, open file
-      editor "$note_path"
-      editor "$note_path/$1.md"
+  if [ $# -eq 1 ]; then
+    # argument was supplied, open file
+    _notar_editor "$note_path"
+    _notar_editor "$note_path/$1.md"
   else
-      # no argument, just open folder
-      editor "$note_path"
+    # no argument, just open folder
+    _notar_editor "$note_path"
   fi
 
 }
 
+# open the notes dir
 notes() {
   subl `notesdir`
 }
 
+# cd into the notes dir
+gnotes() {
+  cd `notesdir`
+}
+
+# open the notes dir in finder
 onotes() {
   open `notesdir`
+}
+
+# search for a note
+fnotes() {
+  _notar_search $1
 }
